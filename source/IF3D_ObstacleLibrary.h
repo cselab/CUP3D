6472642e761547a1da52258571142b5860b4d8c6
Guido Novati <novatig@ethz.ch>
2016-10-10 16:12:41 +0200

    minor

--- source/IF3D_ObstacleLibrary.h
+++ source/IF3D_ObstacleLibrary.h
@@ -2052,7 +2052,7 @@ struct PutFishOnBlocks_Finalize : public GenericLabOperator
 		return (0. < val) - (val < 0.);
 	}
 	template <typename Lab, typename BlockType>
-	void operator()(Lab& lab, const BlockInfo& info, BlockType& b) const
+	void operator()(Lab& lab, const BlockInfo& info, BlockType& b)
 	{
 		if(obstacleBlocks->find(info.blockID) == obstacleBlocks->end()) return;
 		ObstacleBlock* const defblock = obstacleBlocks->find(info.blockID)->second;
