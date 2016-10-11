f25d4dbb6471506b2700dd00124258a35fb339c0
Guido Novati <novatig@gmail.com>
2016-10-11 12:52:12 +0200

    scripts and minor

---
+++ launch/settings2Carlings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASENAME=2CarlingsRe5k_CFL01
+NNODE=8
+NNODEX=8
+NNODEY=1
+FFACTORY=factory2Carlings
+
+OPTIONS=
+OPTIONS+=" -bpdx 128 -bpdy 64 -bpdz 32"
+OPTIONS+=" -2Ddump 1"
+OPTIONS+=" -nprocsx ${NNODEX}"
+OPTIONS+=" -nprocsy ${NNODEY}"
+OPTIONS+=" -nprocsz 1"
+OPTIONS+=" -CFL 0.1"
+OPTIONS+=" -length 0.15"
+OPTIONS+=" -lambda 1e4"
+OPTIONS+=" -nu 0.0000045"
+OPTIONS+=" -tend 20"
--- launch/settingsCarling.sh
+++ launch/settingsCarling.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-BASENAME=FlowPastCarlingFishRe0550_double_spectral_
-NNODE=2
+BASENAME=FlowPastCarlingFishRe0550_CFL001_test2ndOrderDiv
+NNODE=8
+NNODEX=8
+NNODEY=1
 FFACTORY=factoryCarling
 
 OPTIONS=
 OPTIONS+=" -bpdx 64 -bpdy 32 -bpdz 32"
-OPTIONS+=" -2Ddump 1"
-OPTIONS+=" -nprocsx ${NNODE}"
-OPTIONS+=" -CFL 0.1"
+OPTIONS+=" -2Ddump 1 -restart 1"
+OPTIONS+=" -nprocsx ${NNODEX}"
+OPTIONS+=" -nprocsy ${NNODEY}"
+OPTIONS+=" -nprocsz 1"
+OPTIONS+=" -CFL 0.01"
 OPTIONS+=" -length 0.25"
 OPTIONS+=" -lambda 1e4"
 OPTIONS+=" -nu 0.0001136363636"
