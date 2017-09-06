b2f8a29e01605d2dbee1e33ae26e32aca165ec06
Guido Novati <novatig@ethz.ch>
2017-09-06 11:04:25 +0200

    added run scripts for forced startup

---
+++ launch/settingsForcedSphereStartup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASENAME=ForcedSphereStartup_00
+NNODE=2
+FFACTORY=factoryForcedSphereStartup
+
+OPTIONS=
+OPTIONS+=" -bpdx 64 -bpdy 32 -bpdz 32"
+OPTIONS+=" -2Ddump 0"
+OPTIONS+=" -tdump 0.025"
+OPTIONS+=" -nprocsx ${NNODE}"
+OPTIONS+=" -CFL 0.1"
+OPTIONS+=" -uinfx 0.0"
+OPTIONS+=" -length 0.1"
+OPTIONS+=" -lambda 1e5"
+OPTIONS+=" -nu 5e-5"
+OPTIONS+=" -tend 6"
