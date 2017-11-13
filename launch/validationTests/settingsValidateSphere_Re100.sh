b8acaeff50c4ab5e0f448516e5c3990abc538bc1
Sid Verma <sverma@ethz.ch>
2017-11-13 07:08:35 +0100

    sphere validation data added, will serve as basis for code checking from now on

--- launch/validationTests/settingsValidateSphere_Re100.sh
+++ launch/validationTests/settingsValidateSphere_Re100.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# SIM SHOULD GIVE Cd ~ 1.13  (5% OFF AT CURRENT RESOLUTION. SHOULD GET BETTER WITH REFINEMENT)
+
 # https://arc.aiaa.org/doi/pdfplus/10.2514/3.6164
 # http://www.mne.psu.edu/cimbala/me325web_Spring_2012/Labs/Drag/intro.pdf
 # Reference values for comparison
@@ -12,8 +14,8 @@
 # 1330   0.436
 # 2000   0.430
 
-BASENAME=validateSphere_Re50
-NNODE=8
+BASENAME=validateSphere_Re100_smallest
+NNODE=16
 WCLOCK=24:00:00
 FFACTORY=factorySphere
 
@@ -23,8 +25,8 @@ OPTIONS+=" -2Ddump 0"
 OPTIONS+=" -nprocsx ${NNODE}"
 OPTIONS+=" -CFL 0.1"
 OPTIONS+=" -uinfx 0.1"
-OPTIONS+=" -length 0.1"
+OPTIONS+=" -length 0.05"
 OPTIONS+=" -lambda 1e6"
-OPTIONS+=" -nu 0.0002"
+OPTIONS+=" -nu 0.00005"
 OPTIONS+=" -tdump 1"
-OPTIONS+=" -tend 10"
+OPTIONS+=" -tend 30"
