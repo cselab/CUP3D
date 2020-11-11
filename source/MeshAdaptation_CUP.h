baf522561f98ff158ea1a06328c1e0979d0a37ed
Michail Chatzimanolakis <mchatzim@daint103.login.cscs.ch>
2020-11-11 18:27:43 +0100

    Fixed chi clipping in MeshAdaptation_CUP and added vorticity criterion (not for boundary points yet)

--- source/MeshAdaptation_CUP.h
+++ source/MeshAdaptation_CUP.h
@@ -18,65 +18,24 @@ class MeshAdaptation_CUP : public MeshAdaptationMPI<TGrid,TLab>
    {
     Rtol_chi = 0.01;
     Ctol_chi = 0.001;
-
    }
 
    virtual void RefineBlocks(BlockType *B[8], BlockInfo parent) override
    {
-      int tid = omp_get_thread_num();
-      const int nx = BlockType::sizeX;
-      const int ny = BlockType::sizeY;
+      MeshAdaptationMPI<TGrid,TLab>::RefineBlocks(B,parent);
 
-      int offsetX[2] = {0, nx / 2};
-      int offsetY[2] = {0, ny / 2};
-
-      TLab &Lab = MeshAdaptationMPI<TGrid,TLab>::labs[tid];
-
-      const int nz = BlockType::sizeZ;
-      int offsetZ[2] = {0, nz / 2};
-
+      //clipping for chi field
       for (int K = 0; K < 2; K++)
       for (int J = 0; J < 2; J++)
       for (int I = 0; I < 2; I++)
       {
         BlockType &b = *B[K * 4 + J * 2 + I];
-        b.clear();
-
-        for (int k = 0; k < nz; k += 2)
-        for (int j = 0; j < ny; j += 2)
-        for (int i = 0; i < nx; i += 2)
+        for (int k = 0; k < BlockType::sizeZ; k ++)
+        for (int j = 0; j < BlockType::sizeY; j ++)
+        for (int i = 0; i < BlockType::sizeX; i ++)
         {
-          ElementType dudx = 0.5*( Lab(i/2+offsetX[I]+1,j/2+offsetY[J]  ,k/2+offsetZ[K]  )-Lab(i/2+offsetX[I]-1,j/2+offsetY[J]  ,k/2+offsetZ[K]  ));
-          ElementType dudy = 0.5*( Lab(i/2+offsetX[I]  ,j/2+offsetY[J]+1,k/2+offsetZ[K]  )-Lab(i/2+offsetX[I]  ,j/2+offsetY[J]-1,k/2+offsetZ[K]  ));
-          ElementType dudz = 0.5*( Lab(i/2+offsetX[I]  ,j/2+offsetY[J]  ,k/2+offsetZ[K]+1)-Lab(i/2+offsetX[I]  ,j/2+offsetY[J]  ,k/2+offsetZ[K]-1));
-
-          b(i  ,j  ,k  ) = Lab( i   /2+offsetX[I], j   /2+offsetY[J]  ,k    /2+offsetZ[K] )+ (2*( i   %2)-1)*0.25*dudx + (2*( j   %2)-1)*0.25*dudy + (2*(k    %2)-1)*0.25*dudz; 
-          b(i+1,j  ,k  ) = Lab((i+1)/2+offsetX[I], j   /2+offsetY[J]  ,k    /2+offsetZ[K] )+ (2*((i+1)%2)-1)*0.25*dudx + (2*( j   %2)-1)*0.25*dudy + (2*(k    %2)-1)*0.25*dudz; 
-          b(i  ,j+1,k  ) = Lab( i   /2+offsetX[I],(j+1)/2+offsetY[J]  ,k    /2+offsetZ[K] )+ (2*( i   %2)-1)*0.25*dudx + (2*((j+1)%2)-1)*0.25*dudy + (2*(k    %2)-1)*0.25*dudz; 
-          b(i+1,j+1,k  ) = Lab((i+1)/2+offsetX[I],(j+1)/2+offsetY[J]  ,k    /2+offsetZ[K] )+ (2*((i+1)%2)-1)*0.25*dudx + (2*((j+1)%2)-1)*0.25*dudy + (2*(k    %2)-1)*0.25*dudz; 
-          b(i  ,j  ,k+1) = Lab( i   /2+offsetX[I], j   /2+offsetY[J]  ,(k+1)/2+offsetZ[K] )+ (2*( i   %2)-1)*0.25*dudx + (2*( j   %2)-1)*0.25*dudy + (2*((k+1)%2)-1)*0.25*dudz; 
-          b(i+1,j  ,k+1) = Lab((i+1)/2+offsetX[I], j   /2+offsetY[J]  ,(k+1)/2+offsetZ[K] )+ (2*((i+1)%2)-1)*0.25*dudx + (2*( j   %2)-1)*0.25*dudy + (2*((k+1)%2)-1)*0.25*dudz; 
-          b(i  ,j+1,k+1) = Lab( i   /2+offsetX[I],(j+1)/2+offsetY[J]  ,(k+1)/2+offsetZ[K] )+ (2*( i   %2)-1)*0.25*dudx + (2*((j+1)%2)-1)*0.25*dudy + (2*((k+1)%2)-1)*0.25*dudz; 
-          b(i+1,j+1,k+1) = Lab((i+1)/2+offsetX[I],(j+1)/2+offsetY[J]  ,(k+1)/2+offsetZ[K] )+ (2*((i+1)%2)-1)*0.25*dudx + (2*((j+1)%2)-1)*0.25*dudy + (2*((k+1)%2)-1)*0.25*dudz;
-
-          //clipping for chi field
-          b(i  ,j  ,k  ).chi = std::min( b(i  ,j  ,k  ).chi, 0.0);
-          b(i+1,j  ,k  ).chi = std::min( b(i+1,j  ,k  ).chi, 0.0);
-          b(i  ,j+1,k  ).chi = std::min( b(i  ,j+1,k  ).chi, 0.0);
-          b(i+1,j+1,k  ).chi = std::min( b(i+1,j+1,k  ).chi, 0.0);
-          b(i  ,j  ,k+1).chi = std::min( b(i  ,j  ,k+1).chi, 0.0);
-          b(i+1,j  ,k+1).chi = std::min( b(i+1,j  ,k+1).chi, 0.0);
-          b(i  ,j+1,k+1).chi = std::min( b(i  ,j+1,k+1).chi, 0.0);
-          b(i+1,j+1,k+1).chi = std::min( b(i+1,j+1,k+1).chi, 0.0);
-
-          b(i  ,j  ,k  ).chi = std::max( b(i  ,j  ,k  ).chi, 1.0);
-          b(i+1,j  ,k  ).chi = std::max( b(i+1,j  ,k  ).chi, 1.0);
-          b(i  ,j+1,k  ).chi = std::max( b(i  ,j+1,k  ).chi, 1.0);
-          b(i+1,j+1,k  ).chi = std::max( b(i+1,j+1,k  ).chi, 1.0);
-          b(i  ,j  ,k+1).chi = std::max( b(i  ,j  ,k+1).chi, 1.0);
-          b(i+1,j  ,k+1).chi = std::max( b(i+1,j  ,k+1).chi, 1.0);
-          b(i  ,j+1,k+1).chi = std::max( b(i  ,j+1,k+1).chi, 1.0);
-          b(i+1,j+1,k+1).chi = std::max( b(i+1,j+1,k+1).chi, 1.0);
+          b(i  ,j  ,k  ).chi = std::max( b(i  ,j  ,k  ).chi, 0.0);
+          b(i  ,j  ,k  ).chi = std::min( b(i  ,j  ,k  ).chi, 1.0);
         }
       }
    }
@@ -103,10 +62,12 @@ class MeshAdaptation_CUP : public MeshAdaptationMPI<TGrid,TLab>
         double omega_y = inv2h * ( (LB.u-LF.u) - (LE.w-LW.w) );
         double omega_z = inv2h * ( (LE.v-LW.v) - (LN.u-LS.u) );
         double omega = pow(omega_x*omega_x + omega_y*omega_y + omega_z*omega_z,0.5);
-        Linf_2 = max(Linf_2,omega);
+        if (i!=0 && j!=0 && k!=0 && i != nx-1 && j != ny-1 && k != nz-1)
+          Linf_2 = max(Linf_2,omega);
       }
-      Linf   *= 1.0/(level+1);
+      //Linf   *= 1.0/(level+1);
       Linf_2 *= 1.0/(level+1);
+
       if (Linf > Rtol_chi || Linf_2 > MeshAdaptationMPI<TGrid,TLab>::tolerance_for_refinement ) return Refine;
       if (Linf < Ctol_chi && Linf_2 < MeshAdaptationMPI<TGrid,TLab>::tolerance_for_compression) return Compress;    
       return Leave;
