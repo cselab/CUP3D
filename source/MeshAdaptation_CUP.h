5f0052845ee79fb68b8a0a1998d772f9e23474af
Michail Chatzimanolakis <mchatzim@daint106.login.cscs.ch>
2020-11-16 18:02:36 +0100

    Minor change in MeshAdaptation_CUP

--- source/MeshAdaptation_CUP.h
+++ source/MeshAdaptation_CUP.h
@@ -14,7 +14,7 @@ class MeshAdaptation_CUP : public MeshAdaptationMPI<TGrid,TLab>
    typedef typename TGrid::BlockType::ElementType ElementType;
    typedef typename TGrid::BlockType Block;
 
-   MeshAdaptation_CUP(TGrid &grid, double Rtol, double Ctol): MeshAdaptationMPI<TGrid,TLab>(grid,Rtol,Ctol)
+   MeshAdaptation_CUP(TGrid &grid, double Rtol, double Ctol, SimulationData & _s): MeshAdaptationMPI<TGrid,TLab>(grid,Rtol,Ctol)
    {
     Rtol_chi = 0.01;
     Ctol_chi = 0.001;
@@ -30,18 +30,19 @@ class MeshAdaptation_CUP : public MeshAdaptationMPI<TGrid,TLab>
       for (int I = 0; I < 2; I++)
       {
         BlockType &b = *B[K * 4 + J * 2 + I];
-        for (int k = 0; k < BlockType::sizeZ; k ++)
-        for (int j = 0; j < BlockType::sizeY; j ++)
-        for (int i = 0; i < BlockType::sizeX; i ++)
+        for (int iz = 0; iz < BlockType::sizeZ; iz ++)
+        for (int iy = 0; iy < BlockType::sizeY; iy ++)
+        for (int ix = 0; ix < BlockType::sizeX; ix ++)
         {
-          b(i  ,j  ,k  ).chi = std::max( b(i  ,j  ,k  ).chi, 0.0);
-          b(i  ,j  ,k  ).chi = std::min( b(i  ,j  ,k  ).chi, 1.0);
+          b(ix  ,iy  ,iz  ).chi = std::max( b(ix  ,iy  ,iz  ).chi, 0.0);
+          b(ix  ,iy  ,iz  ).chi = std::min( b(ix  ,iy  ,iz  ).chi, 1.0);
         }
       }
    }
 
    virtual State TagLoadedBlock(TLab &Lab_, int level)
    {
+      //This assumes zero Neumann BCs for velocity
       static const int nx = BlockType::sizeX;
       static const int ny = BlockType::sizeY;
       static const int nz = BlockType::sizeZ;
@@ -62,10 +63,8 @@ class MeshAdaptation_CUP : public MeshAdaptationMPI<TGrid,TLab>
         double omega_y = inv2h * ( (LB.u-LF.u) - (LE.w-LW.w) );
         double omega_z = inv2h * ( (LE.v-LW.v) - (LN.u-LS.u) );
         double omega = pow(omega_x*omega_x + omega_y*omega_y + omega_z*omega_z,0.5);
-        if (i!=0 && j!=0 && k!=0 && i != nx-1 && j != ny-1 && k != nz-1)
-          Linf_2 = max(Linf_2,omega);
+        Linf_2 = max(Linf_2,omega);
       }
-      //Linf   *= 1.0/(level+1);
       Linf_2 *= 1.0/(level+1);
 
       if (Linf > Rtol_chi || Linf_2 > MeshAdaptationMPI<TGrid,TLab>::tolerance_for_refinement ) return Refine;
