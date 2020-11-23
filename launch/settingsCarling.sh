4868765d0f138e37f8ac1ee7d5dbe04bbe3cd2da
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2020-11-23 13:25:02 +0100

    Updated Carling fish script

--- launch/settingsCarling.sh
+++ launch/settingsCarling.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-NNODEX=${NNODEX:-4}
+NNODEX=${NNODEX:-16}
 NNODEY=${NNODEY:-1}
 NNODE=$(($NNODEX * $NNODEY))
 
-BPDX=${BPDX:-8}
-BPDY=${BPDY:-$((${BPDX}/2))} #${BPDY:-32}
-BPDZ=${BPDZ:-$((${BPDX}/4))} #${BPDZ:-32}
+BPDX=${BPDX:-16}
+BPDY=${BPDY:-2}
+BPDZ=${BPDZ:-2}
 
 NU=${NU:-0.00001}
 BC=${BC:-freespace}
@@ -15,9 +15,9 @@ FACTORY='CarlingFish L=0.2 T=1.0 xpos=0.3 bFixToPlanar=1 bFixFrameOfRef=1 height
 
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
-OPTIONS+=" -dump2D 1 -dump3D 1 -tdump 0.05 -tend 15 "
+OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 1.0 -tend 10.0 "
 OPTIONS+=" -nslices 2 -slice1_direction 1 -slice2_direction 2 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
-OPTIONS+=" -nprocsx ${NNODEX} -nprocsy ${NNODEY} -nprocsz 1"
-OPTIONS+=" -CFL 0.1 -use-dlm 10 -nu ${NU}"
+OPTIONS+=" -CFL 0.3 -use-dlm 10 -nu ${NU}"
 OPTIONS+=" -levelMax 4 -levelStart 3 -Rtol 1.0 -Ctol 0.1"
+OPTIONS+=" -Advection3rdOrder=true" 
