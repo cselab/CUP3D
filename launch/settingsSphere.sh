f0fbaea686dce7aad0c3f4c3458f910c2df0fcc6
Michail Chatzimanolakis <michaich@eu-login-18.euler.ethz.ch>
2021-02-12 09:02:54 +0100

    Updated sphere launch script

--- launch/settingsSphere.sh
+++ launch/settingsSphere.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 NNODE=64
-DLM=${DLM:-1}
+DLM=${DLM:--1}
 LAMBDA=${LAMBDA:-1e6}
 BPDX=${BPDX:-8}
-BPDY=${BPDY:-8}
-BPDZ=${BPDZ:-8}
+BPDY=${BPDY:-4}
+BPDZ=${BPDZ:-4}
 NU=${NU:-0.00005208333333} # Re 300, to compare against Mimeau Cottet Mortazavi 2016:
 BC=${BC:-dirichlet}
 FACTORY='Sphere L=0.125 xpos=0.125 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1
 '
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
-OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 1.0 -tend 12000 "
+OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 1.0 -tend 100 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
 OPTIONS+=" -CFL 0.1 -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
-OPTIONS+=" -Advection3rdOrder=true"
+OPTIONS+=" -ImplicitPenalization 1"
 OPTIONS+=" -levelMax 4 -levelStart 3 -Rtol 0.40 -Ctol 0.10"
