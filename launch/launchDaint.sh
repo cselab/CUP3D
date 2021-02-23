fe3123966dbfc2552ab7128862f4a575df79e4de
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2021-02-23 09:53:51 +0100

    Updated Daint launch scripts

--- launch/launchDaint.sh
+++ launch/launchDaint.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-SETTINGSNAME=$1
-BASENAME=$2
+
 if [ $# -lt 2 ] ; then
   echo "Usage "$0" SETTINGSNAME BASENAME"
   exit 1
 fi
+SETTINGSNAME=$1
+BASENAME=$2
 
 WCLOCK=${WCLOCK:-24:00:00}
 PARTITION=${PARTITION:-normal}
 EXEC=${EXEC:-simulation}
-
-MYNAME=`whoami`
 BASEPATH="${SCRATCH}/CubismUP3D/"
-#lfs setstripe -c 1 ${BASEPATH}${RUNFOLDER}
 
-if [ ! -f $SETTINGSNAME ]; then
-    echo ${SETTINGSNAME}" not found! - exiting"
-    exit -1
-fi
 source $SETTINGSNAME
 
-NPROCESSORS=$((${NNODE}*12))
 FOLDER=${BASEPATH}${BASENAME}
 mkdir -p ${FOLDER}
 
@@ -30,7 +23,6 @@ cp ../bin/${EXEC} ${FOLDER}/simulation
 cp -r ../source ${FOLDER}/
 cp -r ../Cubism ${FOLDER}/
 cp $0 ${FOLDER}
-
 git diff HEAD > ${FOLDER}/gitdiff.diff
 
 cd ${FOLDER}
@@ -39,34 +31,22 @@ cat <<EOF >daint_sbatch
 #!/bin/bash -l
 
 #SBATCH --account=s929
-##SBATCH --account=eth2
 #SBATCH --job-name="${BASENAME}"
 #SBATCH --output=${BASENAME}_out_%j.txt
 #SBATCH --error=${BASENAME}_err_%j.txt
-
 #SBATCH --time=${WCLOCK}
 #SBATCH --partition=${PARTITION}
 #SBATCH --constraint=gpu
-##SBATCH --constraint=mc
-
 #SBATCH --nodes=${NNODE}
 #SBATCH --ntasks-per-node=1
-# #SBATCH --cpus-per-task=12
-# #SBATCH --threads-per-core=1
-# #SBATCH --mail-user=${MYNAME}@ethz.ch
-# #SBATCH --mail-type=ALL
+#SBATCH --ntasks-per-core=1
+#SBATCH --cpus-per-task=12
+#SBATCH --hint=nomultithread
 
-export MPICH_MAX_THREAD_SAFETY=multiple
-export OMP_PLACES=cores
-export OMP_PROC_BIND=close
-export OMP_NUM_THREADS=12
-#export OMP_NUM_THREADS=36
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun --ntasks ${NNODE} --ntasks-per-node=1 ./simulation ${OPTIONS} -factory-content $(printf "%q" "${FACTORY}")
+srun ./simulation ${OPTIONS} -factory-content $(printf "%q" "${FACTORY}")
 
 EOF
 chmod 755 daint_sbatch
 sbatch daint_sbatch
-
-#export OMP_NUM_THREADS=36
-#srun --ntasks ${NNODE} --ntasks-per-node=1 ./simulation ${OPTIONS} -factory-content ${FACTORY}
--- launch/launchDaint_mc.sh
+++ launch/launchDaint_mc.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-SETTINGSNAME=$1
-BASENAME=$2
+
 if [ $# -lt 2 ] ; then
   echo "Usage "$0" SETTINGSNAME BASENAME"
   exit 1
 fi
+SETTINGSNAME=$1
+BASENAME=$2
 
 WCLOCK=${WCLOCK:-24:00:00}
 PARTITION=${PARTITION:-normal}
 EXEC=${EXEC:-simulation}
-
-MYNAME=`whoami`
 BASEPATH="${SCRATCH}/CubismUP3D/"
-#lfs setstripe -c 1 ${BASEPATH}${RUNFOLDER}
 
-if [ ! -f $SETTINGSNAME ]; then
-    echo ${SETTINGSNAME}" not found! - exiting"
-    exit -1
-fi
 source $SETTINGSNAME
 
 FOLDER=${BASEPATH}${BASENAME}
@@ -27,13 +21,11 @@ cp $SETTINGSNAME ${FOLDER}/settings.sh
 [[ -n "${FFACTORY}" ]] && cp ${FFACTORY} ${FOLDER}/factory
 cp ../bin/${EXEC} ${FOLDER}/simulation
 cp -r ../source ${FOLDER}/
+cp -r ../Cubism ${FOLDER}/
 cp $0 ${FOLDER}
-
 git diff HEAD > ${FOLDER}/gitdiff.diff
 
 cd ${FOLDER}
-
-NNODE=$(($NPROCX/2))
 
 cat <<EOF >daint_sbatch
 #!/bin/bash -l
@@ -42,27 +34,19 @@ cat <<EOF >daint_sbatch
 #SBATCH --job-name="${BASENAME}"
 #SBATCH --output=${BASENAME}_out_%j.txt
 #SBATCH --error=${BASENAME}_err_%j.txt
-
 #SBATCH --time=${WCLOCK}
 #SBATCH --partition=${PARTITION}
-
+#SBATCH --constraint=mc
 #SBATCH --nodes=${NNODE}
 #SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-core=1
 #SBATCH --cpus-per-task=18
-#SBATCH --threads-per-core=1
-#SBATCH --constraint=mc
-#SBATCH --mail-user=${MYNAME}@ethz.ch
-#SBATCH --mail-type=ALL
+#SBATCH --hint=nomultithread
 
-module swap daint-gpu daint-mc
-module swap gcc gcc/7.1.0
-module load GSL cray-hdf5-parallel
-module load fftw
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-export MPICH_MAX_THREAD_SAFETY=multiple
-export OMP_NUM_THREADS=18
-srun --ntasks ${NPROCX} --threads-per-core=1 --ntasks-per-node=2 --cpus-per-task=18 time ./simulation ${OPTIONS} -factory-content $(printf "%q" "${FACTORY}")
+srun ./simulation ${OPTIONS} -factory-content $(printf "%q" "${FACTORY}")
+
 EOF
-
 chmod 755 daint_sbatch
 sbatch daint_sbatch
