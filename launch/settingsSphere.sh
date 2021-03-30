2779654b99ebf2a39d07fe0d5e3b6b99dd1c7ffb
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2021-03-30 14:24:51 +0200

    sphere script update

--- launch/settingsSphere.sh
+++ launch/settingsSphere.sh
@@ -2,7 +2,7 @@
 NNODE=64
 DLM=${DLM:--1}
 LAMBDA=${LAMBDA:-1e6}
-BPDX=${BPDX:-16}
+BPDX=${BPDX:-32}
 BPDY=${BPDY:-8}
 BPDZ=${BPDZ:-8}
 NU=${NU:-0.00005208333333} # Re 300, to compare against Mimeau Cottet Mortazavi 2016
@@ -13,8 +13,12 @@ OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
 OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 1.0 -tend 100 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
-OPTIONS+=" -CFL 0.4 -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
+OPTIONS+=" -CFL 0.25 -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
 OPTIONS+=" -ImplicitPenalization 1"
-OPTIONS+=" -levelMax 4 -levelStart 2 -Rtol 0.40 -Ctol 0.10"
-OPTIONS+=" -extentx 2.0 "
+OPTIONS+=" -levelMax 4 -levelStart 3 -Rtol 0.40 -Ctol 0.10"
+OPTIONS+=" -extentx 4.0 "
 OPTIONS+=" -TimeOrder 2 "
+
+####
+#### For a smaller domain use BPDX=16 and -extentx 2.0
+####
