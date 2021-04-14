ed902b4b703992d1dd0b9a5421aac7eed40ed23b
Pascal Weber <webepasc@student.ethz.ch>
2021-04-14 21:15:19 +0200

    setting sphere

--- launch/settingsSphere.sh
+++ launch/settingsSphere.sh
@@ -2,19 +2,22 @@
 NNODE=64
 DLM=${DLM:--1}
 LAMBDA=${LAMBDA:-1e6}
-BPDX=${BPDX:-16}
-BPDY=${BPDY:-4}
-BPDZ=${BPDZ:-4}
-NU=${NU:-0.00005208333333} # Re 300, to compare against Mimeau Cottet Mortazavi 2016
+BPDX=${BPDX:-32} #16
+BPDY=${BPDY:-8}
+BPDZ=${BPDZ:-8}
+LEVELMAX=${LEVELMAX:-4}
+# Re=300 <-> NU=0.00005208333333; Re=500 <-> NU=0.00003125; Re=1000 <-> NU=0.000015625; Re=10'000 <-> NU=0.0000015625; Re=420'000 <-> NU=0.000000037202381; Re=1'140'000 <-> NU=0.00000001370614
+NU=${NU:-0.00000001370614}
+
 BC=${BC:-dirichlet}
-FACTORY='Sphere L=0.125 xpos=0.6 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1
-'
+FACTORY='Sphere L=0.125 xpos=0.6 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1'
+
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
 OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 1.0 -tend 100 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
-OPTIONS+=" -CFL 0.4 -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
+OPTIONS+=" -CFL 0.25 -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
 OPTIONS+=" -ImplicitPenalization 1"
-OPTIONS+=" -levelMax 4 -levelStart 3 -Rtol 0.40 -Ctol 0.10"
-OPTIONS+=" -extentx 2.0 "
+OPTIONS+=" -levelMax $LEVELMAX -levelStart 3 -Rtol 0.40 -Ctol 0.10"
+OPTIONS+=" -extentx 4.0 " #-extentx 2.0
 OPTIONS+=" -TimeOrder 2 "
