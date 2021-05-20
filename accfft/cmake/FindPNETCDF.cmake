fdf328cb0bf7c8dce94916f8272efdfe3aafb881
Michail Chatzimanolakis <mchatzim@daint106.login.cscs.ch>
2021-05-20 08:16:27 +0200

    removed old fft library

--- accfft/cmake/FindPNETCDF.cmake
+++
@@ -1,9 +0,0 @@
-#######################################################################
-#
-#  CMake Module that uses the BASIC_FIND macro to find the PIO
-#  library and include directory
-#
-#######################################################################
-
-include(basicFind)
-BASIC_FIND (PNETCDF "pnetcdf.h" "pnetcdf")
--- accfft/doc/README.md
+++
@@ -1,5 +0,0 @@
-My Main Page                         {#mainpage}
-============
-
-To access the manual for each function click on the *Files* tab above.
-You can also download a pdf version of the document [here](http://accfft.org/doxygen/manual.pdf).
