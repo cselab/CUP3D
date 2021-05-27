f7ce7156c2fa234ce566d5ce52980f694ee82c23
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2021-05-27 10:19:07 +0200

    minor

--- launch/settingsDcyl.sh
+++ launch/settingsDcyl.sh
@@ -6,6 +6,7 @@ BPDY=${BPDY:-8}
 BPDZ=${BPDZ:-8}
 NU=${NU:-0.0002} #Re=U*L/nu = 1*0.1/nu = 500
 
+# This follows the paper "Iterative Brinkman penalization for simulation of impulsively started flow past a sphere and a circular disc" (2017)
 # L is the diameter.
 # We set the thickness equal to L/16 (halflength = thickness/2)
 FACTORY='Cylinder L=0.1 xpos=2.0 ypos=2.0 zpos=2.0 xvel=0.0 yvel=0.0 zvel=1.0 bFixFrameOfRef=1 bForcedInSimFrame=1 halflength=0.003125
