3edf084719538b32f312f887cbafed6f170370ac
Michail Chatzimanolakis <mchatzim@daint102.login.cscs.ch>
2021-05-31 14:24:41 +0200

    Coin run script corrected

--- launch/settingsDcyl.sh
+++ launch/settingsDcyl.sh
@@ -9,15 +9,15 @@ NU=${NU:-0.0002} #Re=U*L/nu = 1*0.1/nu = 500
 # This follows the paper "Iterative Brinkman penalization for simulation of impulsively started flow past a sphere and a circular disc" (2017)
 # L is the diameter.
 # We set the thickness equal to L/16 (halflength = thickness/2)
-FACTORY='Cylinder L=0.1 xpos=2.0 ypos=2.0 zpos=2.0 xvel=0.0 yvel=0.0 zvel=1.0 bFixFrameOfRef=1 bForcedInSimFrame=1 halflength=0.003125
+FACTORY='Cylinder L=0.1 xpos=1.0 ypos=1.0 zpos=1.0 xvel=0.0 yvel=0.0 zvel=1.0 bFixFrameOfRef=1 bForcedInSimFrame=1 halflength=0.003125
 '
 
 OPTIONS=
-OPTIONS+=" -extentx 4.0"
+OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
+OPTIONS+=" -extentx 2.0"
 OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 0.1 -tend 20.0 "
-OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
 OPTIONS+=" -CFL 0.3 -use-dlm -10 -nu ${NU}"
-OPTIONS+=" -levelMax 6 -levelStart 3 -Rtol 0.5 -Ctol 0.05"
+OPTIONS+=" -levelMax 6 -levelStart 4 -Rtol 0.5 -Ctol 0.05"
 OPTIONS+=" -implicitPenalization 1"
 OPTIONS+=" -TimeOrder 2"
 OPTIONS+=" -dumpP 1"
