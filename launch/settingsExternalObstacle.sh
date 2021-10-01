c4d753259cc3c91a2fba49a5eb214d2c5b0a5617
Michail Chatzimanolakis <mchatzim@daint102.login.cscs.ch>
2021-10-02 01:32:01 +0200

    External Obstacle launchscript modified

--- launch/settingsExternalObstacle.sh
+++ launch/settingsExternalObstacle.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 NNODE=${NNODE:-64}
-DLM=${DLM:--1}
 LAMBDA=${LAMBDA:-1e6}
-BPDX=${BPDX:-2} #16
+BPDX=${BPDX:-16}
 BPDY=${BPDY:-2}
 BPDZ=${BPDZ:-2}
 LEVELS=${LEVELS:-7}
 CFL=${CFL:-0.2} # if 0, DT is used
-DT=${DT:-1e-4}
 PT=${PT:-1e-8}
 PTR=${PTR:-1e-4}
-# Re=100 <-> 0.00125
-NU=${NU:-0.000625}
+NU=${NU:-0.00000625} #Re=10,000
 BC=${BC:-dirichlet}
-FACTORY='ExternalObstacle L=0.5 xpos=0.5 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/pweber/korali/examples/study.cases/CUP3D/_deps/CUP-3D/launch/externalObstacles/Robot.ply
+FACTORY='ExternalObstacle L=0.5 xpos=0.5 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/mchatzim/AMR_codes/3D_CUP/launch/externalObstacles/Robot.ply
 '
-
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
-OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 0 -tend 0 -nsteps 1" # -fdump 1
+OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 0.1 -tend 1000.0 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
-OPTIONS+=" -CFL ${CFL} -dt ${DT} -lambda ${LAMBDA} -use-dlm ${DLM} -nu ${NU}"
+OPTIONS+=" -CFL ${CFL} -lambda ${LAMBDA} -nu ${NU}"
 OPTIONS+=" -ImplicitPenalization 1"
-OPTIONS+=" -levelMax ${LEVELS} -Rtol 4.00 -Ctol 1.00" #-levelStart 3 
-OPTIONS+=" -extentx 1.0 " # -extentx 2.0
+OPTIONS+=" -levelMax ${LEVELS} -Rtol 4.00 -Ctol 1.00 -levelStart 3"
+OPTIONS+=" -extentx 8.0 "
 OPTIONS+=" -TimeOrder 2 "
 OPTIONS+=" -poissonTol ${PT} -poissonTolRel ${PTR} "
-OPTIONS+=" -dumpOmegaX 1 -dumpOmegaY 1 dumpOmegaZ 1"
+OPTIONS+=" -dumpOmegaX 0 -dumpOmegaY 0 dumpOmegaZ 0"
 OPTIONS+=" -dumpVelocityX 1 -dumpVelocityY 1 -dumpVelocityZ 1"
