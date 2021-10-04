a451ea656019f6e922fc01d92d95d6ad48e6ade4
Michail Chatzimanolakis <mchatzim@daint105.login.cscs.ch>
2021-10-04 16:59:36 +0200

    The fish should swim with its head first not its tail you moron

--- launch/settingsExternalObstacle.sh
+++ launch/settingsExternalObstacle.sh
@@ -10,7 +10,7 @@ PT=${PT:-1e-8}
 PTR=${PTR:-1e-4}
 NU=${NU:-0.00000625} #Re=10,000
 BC=${BC:-dirichlet}
-FACTORY='ExternalObstacle L=0.5 xpos=0.5 xvel=0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/mchatzim/AMR_codes/3D_CUP/launch/externalObstacles/Robot.ply
+FACTORY='ExternalObstacle L=0.5 xpos=0.5 xvel=-0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/mchatzim/AMR_codes/3D_CUP/launch/externalObstacles/Robot.ply
 '
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
@@ -18,7 +18,7 @@ OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 0.1 -tend 1000.0 "
 OPTIONS+=" -BC_x ${BC} -BC_y ${BC} -BC_z ${BC}"
 OPTIONS+=" -CFL ${CFL} -lambda ${LAMBDA} -nu ${NU}"
 OPTIONS+=" -ImplicitPenalization 1"
-OPTIONS+=" -levelMax ${LEVELS} -Rtol 0.50 -Ctol 0.05 -levelStart 3"
+OPTIONS+=" -levelMax ${LEVELS} -Rtol 4.00 -Ctol 0.05 -levelStart 3"
 OPTIONS+=" -extentx 8.0 "
 OPTIONS+=" -TimeOrder 2 "
 OPTIONS+=" -poissonTol ${PT} -poissonTolRel ${PTR} "
