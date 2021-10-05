644a20a2a9df91d692c58b935cb5e431fe525e1b
Michail Chatzimanolakis <mchatzim@daint104.login.cscs.ch>
2021-10-05 21:00:40 +0200

    changed robot fish launch script

--- launch/settingsExternalObstacle.sh
+++ launch/settingsExternalObstacle.sh
@@ -2,15 +2,15 @@
 NNODE=${NNODE:-64}
 LAMBDA=${LAMBDA:-1e6}
 BPDX=${BPDX:-16}
-BPDY=${BPDY:-4}
-BPDZ=${BPDZ:-4}
+BPDY=${BPDY:-2}
+BPDZ=${BPDZ:-2}
 LEVELS=${LEVELS:-8}
-CFL=${CFL:-0.2} # if 0, DT is used
-PT=${PT:-1e-8}
+CFL=${CFL:-0.7} # if 0, DT is used
+PT=${PT:-1e-7}
 PTR=${PTR:-1e-4}
 NU=${NU:-0.00000625} #Re=10,000
 BC=${BC:-dirichlet}
-FACTORY='ExternalObstacle L=0.5 xpos=0.5 xvel=-0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/mchatzim/AMR_codes/3D_CUP/launch/externalObstacles/Robot.ply
+FACTORY='ExternalObstacle L=0.5 xpos=7.5 xvel=-0.125 bForcedInSimFrame=1 bFixFrameOfRef=1 externalObstaclePath=/users/mchatzim/AMR_codes/3D_CUP/launch/externalObstacles/Robot.ply
 '
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
