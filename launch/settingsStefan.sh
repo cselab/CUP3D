e4bcb2136b59c5000a98f58f53fab12b1dce82ae
Michail Chatzimanolakis <mchatzim@daint103.login.cscs.ch>
2022-03-08 10:00:48 +0100

    Minor fix in launch script for StefanFish

--- launch/settingsStefan.sh
+++ launch/settingsStefan.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 NNODE=${NNODE:-64}
-BPDX=${BPDX:-16}
-BPDY=${BPDY:-8}
-BPDZ=${BPDZ:-8}
-LEVELS=${LEVELS:-5}
-CFL=${CFL:-0.2} # if 0, DT is used
 PT=${PT:-1e-8}
 PTR=${PTR:-1e-4}
 
@@ -14,8 +9,9 @@ NU=${NU:-0.00004}
 FACTORY='StefanFish L=0.2 T=1 xpos=0.3 bFixToPlanar=1 bFixFrameOfRef=1 Correct=1 heightProfile=danio widthProfile=stefan'
 
 OPTIONS=
+OPTIONS+=" -bpdx 16 -bpdy 8 -bpdz 8"
 OPTIONS+=" -tdump 0.1 -tend 5"
-OPTIONS+=" -CFL ${CFL} -nu ${NU}"
-OPTIONS+=" -levelMax ${LEVELS} -levelStart 1 -Rtol 4.00 -Ctol 1.00"
+OPTIONS+=" -CFL 0.4 -nu ${NU}"
+OPTIONS+=" -levelMax 5 -levelStart 1 -Rtol 4.00 -Ctol 1.00"
 OPTIONS+=" -extentx 2.0 "
 OPTIONS+=" -poissonTol ${PT} -poissonTolRel ${PTR} "
