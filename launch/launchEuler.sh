288a76eff9f93a7405dbf8051024e8841837603b
Michail Chatzimanolakis <michaich@eu-login-15.euler.ethz.ch>
2022-09-15 10:25:27 +0200

    launch script for Euler minor change

--- launch/launchEuler.sh
+++ launch/launchEuler.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
-SETTINGSNAME=$1
-BASENAME=$2
 if [ $# -lt 2 ] ; then
   echo "Usage "$0" SETTINGSNAME BASENAME"
   exit 1
 fi
+SETTINGSNAME=$1
+BASENAME=$2
 BASEPATH=${SCRATCH}/CubismUP3D/
 
-NTHREADS=1
-
-if [ ! -f $SETTINGSNAME ];then
-    echo ${SETTINGSNAME}" not found! - exiting"
-    exit -1
-fi
 source $SETTINGSNAME
-
-NPROCESSORS=$((${NNODE}*${NTHREADS}))
 FOLDER=${BASEPATH}${BASENAME}
 mkdir -p ${FOLDER}
-
 cp $SETTINGSNAME ${FOLDER}/settings.sh
 [[ -n "${FFACTORY}" ]] && cp ${FFACTORY} ${FOLDER}/factory
 cp ../bin/simulation ${FOLDER}/simulation
+cp -r ../source ${FOLDER}/
 
 cd $FOLDER
-
 unset LSB_AFFINITY_HOSTFILE
-export OMP_NUM_THREADS=1 #${NTHREADS}
+export MV2_ENABLE_AFFINITY=0
+export OMP_NUM_THREADS=1
 echo $OPTIONS > settings.txt
 
-#bsub -J ${BASENAME} -W 24:00 -n 128 "unset LSB_AFFINITY_HOSTFILE; mpirun -n 128 --map-by core:PE=1 ./simulation ${OPTIONS} -factory-content \"${FACTORY}\""
+if [ "${RUNLOCAL}" == "true" ] ; then
+mpirun -n 360 --map-by core:PE=1 ./simulation ${OPTIONS} -factory-content "${FACTORY}"
+else
 bsub -J ${BASENAME} -W 24:00 -R "select[model==XeonGold_6150]fullnode" -n 360 "unset LSB_AFFINITY_HOSTFILE; mpirun -n 360 --map-by core:PE=1 ./simulation ${OPTIONS} -factory-content \"${FACTORY}\""
-
+fi
