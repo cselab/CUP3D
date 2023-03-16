2fee700b4748a30cb50403b3861f343f28f2a65b
Pascal Weber <webepasc@student.ethz.ch>
2023-03-16 11:15:09 +0200

    adjusted channel setting to AMR

--- launch/settingsChannel.sh
+++ launch/settingsChannel.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-NNODEX=${NNODEX:-4}
-NNODEY=1
-NNODE=$(($NNODEX * $NNODEY))
 
-BPDX=${BPDX:-32}
-BPDY=${BPDY:-${BPDX}} #${BPDY:-32}
-BPDZ=${BPDZ:-$((${BPDX}/8))} #${BPDZ:-32}
+NNODE=4
+BPDX=${BPDX:-16}
+BPDY=${BPDY:-${BPDX}}
+BPDZ=${BPDZ:-$((${BPDX}/8))}
 
 NU=${NU:-0.1} # RE = 8/12 Ly Umax / \nu = 66.666
 
@@ -13,8 +11,16 @@ FACTORY=''
 
 OPTIONS=
 OPTIONS+=" -bpdx ${BPDX} -bpdy ${BPDY} -bpdz ${BPDZ}"
-OPTIONS+=" -dump2D 0 -dump3D 1 -tdump 0.1"
-OPTIONS+=" -BC_x periodic -BC_y wall -BC_z periodic -initCond channel -fixedMassFlux 1"
-OPTIONS+=" -nprocsx ${NNODEX} -nprocsy ${NNODEY} -nprocsz 1"
-OPTIONS+=" -CFL 0.1 -tend 10 -uMax_forced 1 -compute-dissipation 1"
+OPTIONS+=" -tdump 0.1 -tend 100.00 "
+OPTIONS+=" -CFL 0.3 -nu ${NU} -implicitDiffusion 1"
+OPTIONS+=" -BC_x periodic -BC_y wall -BC_z periodic -initCond channel -fixedMassFlux 0 -uMax_forced 1"
 OPTIONS+=" -nu ${NU}"
+OPTIONS+=" -dumpP 1"
+OPTIONS+=" -dumpOmega 1"
+OPTIONS+=" -dumpOmegaX 1"
+OPTIONS+=" -dumpOmegaY 1"
+OPTIONS+=" -dumpOmegaZ 1"
+OPTIONS+=" -dumpVelocity 1"
+OPTIONS+=" -dumpVelocityX 1"
+OPTIONS+=" -dumpVelocityY 1"
+OPTIONS+=" -dumpVelocityZ 1"
