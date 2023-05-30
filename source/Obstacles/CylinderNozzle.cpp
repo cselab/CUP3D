98c6d24e2d0c25b335d8874aab2ee4a89242b9f6
michaich <michaich@ethz.ch>
2023-05-30 23:38:47 +0300

    minor

--- source/Obstacles/CylinderNozzle.cpp
+++ source/Obstacles/CylinderNozzle.cpp
@@ -157,7 +157,7 @@ std::vector<Real> CylinderNozzle::state(const int agentID)
       const Real x = block->pX[i] - position[0];
       const Real y = block->pY[i] - position[1];
       const Real z = block->pZ[i] - position[2];
-      if (std::fabs(z) > 0.75*halflength) continue;
+      if (std::fabs(z) > 0.99*halflength) continue;
 
       Real theta = atan2(y,x);
       if (theta < 0) theta += 2.*M_PI;
