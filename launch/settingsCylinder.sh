ddf017141dcff7716cc3dbc401d0d618fb857d3b
Michail Chatzimanolakis <mchatzim@daint101.login.cscs.ch>
2023-10-23 13:30:38 +0200

    added script to run cylinder with periodic BCs in spanwiese direction

--- launch/settingsCylinder.sh
+++ launch/settingsCylinder.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
-NNODE=64
-PSOLVER="cuda_iterative"
-# L is the diameter.
-FACTORY='Cylinder L=0.2 xpos=0.5 xvel=0.2 yvel=0.0 zvel=0.0 bFixFrameOfRef=1 bForcedInSimFrame=1 halflength=0.5'
-OPTIONS=
-OPTIONS+=" -bpdx 8 -bpdy 4 -bpdz 4"
-OPTIONS+=" -extentx 2.0"
-OPTIONS+=" -poissonSolver ${PSOLVER}"
-OPTIONS+=" -tdump 0.1 -tend 200 "
-OPTIONS+=" -CFL 0.5 -nu 0.00001 "
-OPTIONS+=" -levelMax 6 -levelStart 4 -Rtol 5.0 -Ctol 0.5"
-OPTIONS+=" -lambda 1e12"
-OPTIONS+=" -dumpP 1"
-OPTIONS+=" -dumpChi 1"
-OPTIONS+=" -dumpOmega 1"
-OPTIONS+=" -dumpOmegaX 1"
-OPTIONS+=" -dumpOmegaY 1"
-OPTIONS+=" -dumpOmegaZ 1"
-OPTIONS+=" -dumpVelocity 1"
-OPTIONS+=" -dumpVelocityX 1"
-OPTIONS+=" -dumpVelocityY 1"
-OPTIONS+=" -dumpVelocityZ 1"
+NNODE=64 #number of nodes to use for a simulation
+
+#----------------------------------
+#Settings for cylinder
+#----------------------------------
+XPOS=${XPOS:-1.0}      #cylinder center (x-coordinate)
+XVEL=${XVEL:-0.2}      #cylinder velocity (x-component)
+DIAMETER=${DIAMETER:-0.1}  #cylinder diameter
+HALFLENGTH=${HALFLENGTH:-1.25}  #cylinder halflength
+
+#----------------------------------
+#Settings for pressure equation 
+#----------------------------------
+#PSOLVER="iterative"       #CPU solver
+PSOLVER="cuda_iterative"  #GPU solver
+PT=${PT:-1e-8}            #absolute error tolerance
+PTR=${PTR:-0}             #relative error tolerance
+
+#----------------------------------
+#Settings for simulation domain
+#----------------------------------
+EXTENT=${EXTENT:-4}    #length of largest side
+BPDX=${BPDX:-4}        #number of blocks in x-side, at refinement level = 0
+BPDY=${BPDY:-2}        #number of blocks in y-side, at refinement level = 0
+BPDZ=${BPDZ:-2}        #number of blocks in z-side, at refinement level = 0
+#
+# Coarsest possible mesh (at refinement level = 0) is a
+# (BPDX * BS) x (BPDY * BS) x (BPDZ * BS) grid, where BS is the number 
+# of grid points per block (equal to 8, by default).
+
+#--------------------------------------
+#Settings for Adaptive Mesh Refinement
+#--------------------------------------
+RTOL=${RTOL:-10.}              #grid is refined when curl(u) > Rtol (u: fluid velocity)
+CTOL=${CTOL:-1.}               #grid is compressed when curl(u) < Ctol (u: fluid velocity)
+LEVELS=${LEVELS:-8}            #maximum number of refinement levels allowed
+LEVELSSTART=${LEVELSSTART:-4}  #at t=0 the grid is uniform and at this refinement level. Must be strictly less than LEVELS.
+
+#--------------------------------------
+#Other settings
+#--------------------------------------
+NU=${NU:-0.000010} #fluid viscosity
+#The Reynolds number is defined as Re = XVEL * DIAMETER / NU and can be controlled by
+#modifying the values of NU. Here are some examples:
+# Re=1000  <-> NU=0.000040
+# Re=2000  <-> NU=0.000020
+# Re=4000  <-> NU=0.000010
+
+#--------------------------------------
+#Timestep and file saving
+#--------------------------------------
+TDUMP=${TDUMP:-0.1}   #Save files for t = i*TDUMP, i=0,1,...
+TEND=${TEND:-100.}    #Perform simulation until t=TEND
+CFL=${CFL:-0.50}      #Courant number: controls timestep size (should not exceed 1.0).
+VERBOSE=${VERBOSE:-1} #Set to 1 for more verbose screen output.
+
+OPTIONS="-bpdx $BPDX -bpdy $BPDY -bpdz $BPDZ -levelMax $LEVELS -levelStart $LEVELSSTART -Rtol $RTOL -Ctol $CTOL -extentx $EXTENT -CFL $CFL -tdump $TDUMP -nu $NU -tend $TEND -verbose $VERBOSE -poissonTol $PT -poissonTolRel $PTR -poissonSolver $PSOLVER"
+OPTIONS+=" -bMeanConstraint 0 -BC_z periodic " #hardcoded periodic BCs in spanwise direction
+FACTORY="CylinderNozzle xpos=$XPOS bFixFrameOfRef=1 bForcedInSimFrame=1 xvel=$XVEL L=$DIAMETER halflength=$HALFLENGTH ";
